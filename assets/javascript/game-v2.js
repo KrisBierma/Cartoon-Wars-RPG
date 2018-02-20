ed87f1ab67661d9ef4ea1b63fe633e3d8ffe06fc
Kris <kris.bierma@gmail.com>
2018-02-20 15:58:37 -0600

    working on hp

--- assets/javascript/game-v2.js
+++ assets/javascript/game-v2.js
@@ -3,31 +3,35 @@ $(document).ready(function(){
 var marvin = {
     name:"marvin",
     location: $("#marvin"),
-    hp: 100,
+    hp: 50,
+    hpLocation: $("#marvin-hp"),
     damage: 8
     }
 var pepe = {
     name:"pepe",
     location: $("#pepe"),
-    hp:120,
+    hp:75,
+    hpLocation: $("#pepe-hp"),
     damage: 12
     }
 var popeye = {
     name:"popeye",
     location: $("#popeye"),
-    hp:140,
+    hp:100,
+    hpLocation: $("#popeye-hp"),
     damage: 16
     }
 var rosie = {
     name:"rosie",
     location: $("#rosie"),
-    hp:160,
+    hp:125,
+    hpLocation: $("#rosie-hp"),
     damage: 20
     }
 
-var storage=[marvin, pepe, popeye, rosie];
-
-console.log(storage[0].location);
+var info=[marvin, pepe, popeye, rosie];
+//storage is now info
+console.log(info[0].location);
 ///var marvin = $("#marvin"); 
 ///var pepe=$("#pepe");
 ///var popeye = $("#popeye");
@@ -35,39 +39,36 @@ console.log(storage[0].location);
 ///var storage=[marvin, pepe, popeye, rosie];
 ///var charId=["marvin", "pepe", "popeye", "rosie"];
 
-// var threeEnemies =[];
-// var run=0;
 var isCharChosen;
 var isDefenderChosen;
+var isDoneAttacking;
 var currentDefender;
-var defenderHp;
-var charHp;
+var currentChar;
 
 function initializeGame(){
     currentDefender="";
+    currentChar="";
     isCharChosen=false;
     isDefenderChosen=false;
+    isDoneAttacking=false;
     $("#enemies", "#fight", "#defender").empty();
     };  
 
 $(".charBox").on("click", function(event){
     if (isCharChosen) return;
     console.log(event.currentTarget.id); //delete later
-    //if (!isDefenderChosen);
     for (var i=0; i<4; i++){
-        $(storage[i].location).detach();
+        $(info[i].location).detach();
     
-        if (charId[i]===event.currentTarget.id){
-            $("#yourChar").append(storage[i]);
+        if (info[i].name===event.currentTarget.id){
+            $("#yourChar").append(info[i].location);
+            currentChar=info[i];
             isCharChosen=true;
         }
         else {
-            storage[i].addClass("charBox-enemies");
-            $("#enemies").append(storage[i]);
-            console.log(storage[i]);
-            // console.log(marvin);
-            //threeEnemies[i]=storage[i];
-            //console.log(threeEnemies);
+            (info[i].location).addClass("charBox-enemies");
+            $("#enemies").append(info[i].location);
+            console.log(info[i].location);
             }
         }
     }); //end .charBox click
@@ -81,13 +82,13 @@ $(".blah").on("click", function(){
     console.log(event.currentTarget.id); //delete later
     if (isDefenderChosen) return;
     for (var j=0; j<4; j++){
-        if (charId[j]===event.currentTarget.id){
-            $(storage[j]).detach();
-            $("#defender").append(storage[j]);
-            currentDefender=charId[j];
+        if (info[j].name===event.currentTarget.id){
+            $(info[j].location).detach();
+            $("#defender").append(info[j].location);
+            currentDefender=info[j];
             //defenderHp=...value; //NEED THIS
-            // storage[j].addClass("charBox-defender");
-            storage[j].removeClass("charBox-enemies");
+            // info[j].addClass("charBox-defender");
+            info[j].location.removeClass("charBox-enemies");
             isDefenderChosen=true;
             }
         }
@@ -95,31 +96,56 @@ $(".blah").on("click", function(){
 ); //end .charBox-enemies click
 
 $("#attack").on("click", function(){
+    var charCapName = capFirstLetter(currentDefender.name);
+    console.log(currentDefender);
+    console.log(charCapName);
     console.log("attack button running");
     if (!isCharChosen) return;
     if (!isDefenderChosen) return;
-    $("#attackMsg").html("You attacked "+ currentDefender+" for "+"" +" damage." + "<br>"
-    +currentDefender + " attacked you for "+""+" damage.");
     
-    //reduce hp of char
-    //.text of char hp 
-    //reduce hp of defender
-    //.text of defender hp
-    //if char hp =0, lose game; "You've been defeated...GAME OVER" & button "restart"
+    if (currentChar.hp>0 && currentDefender.hp>0){
+        if (isDoneAttacking) {
+            return;
+            }
+        else {
+            $("#attackMsg").html("You attacked " + charCapName +" for " + currentChar.damage + " damage." + "<br>"
+            + charCapName + " attacked you back for " + currentDefender.damage + " damage.");
+            currentChar.hp = currentChar.hp - currentDefender.damage;  
+            $(currentChar.hpLocation).text(currentChar.hp);
+            currentDefender.hp = currentDefender.hp - currentChar.damage;
+            $(currentDefender.hpLocation).text(currentDefender.hp);
+            currentChar.damage += currentChar.damage;
+            console.log(currentChar.damage);
+            }
+        }
 
+    else if (currentChar.hp<=0){
+        if (isDoneAttacking) {
+            $("#attackMsg").html("No enemy here.");
+            return;}
+        $("#attackMsg").html("You've been defeated...GAME OVER");
+        $("<button>").append;
+        
+        //if char hp =0, lose game; "You've been defeated...GAME OVER" & button "restart"
+        }
+    else if (currentDefender.hp<=0){
+        if (isDoneAttacking) {
+            $("#attackMsg").html("No enemy here.");
+            return;}
+        $("#attackMsg").html("You have defeated " + charCapName +". Choose another enemy to fight.");
+        isDoneAttacking = true;
+        //
+        }
 
-    //script shows up "You attacked snoopy for 8 damage. <br> snoopy attacked you back for 25 damage"
-//each attack, char increase attack by that number 8, 16, 24. enemy attack stays the same 25
-//if enemy hp is 0 it's removed.
+function capFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+    }    
 })
 
 initializeGame();
    }); //end document.ready
 
-
-
 //text reads "You have defeated Marvin. Choose another enemy to fight."
-//if attack is clicked again "No enemy here."
 //choose another character
 //char attack power is still increasing
 //You won! Game over & restart button
