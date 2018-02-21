dc1127db5bffd2db579c874e3359afddc97dcee4
Kris <kris.bierma@gmail.com>
2018-02-21 15:18:52 -0600

    finished with initialize

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# week-4-game-RPG
-RPG game using JQuery
+# week-4-game-Cartoon Wars RPG
+
+This is a simple Role Playing Game where the user's character attacks other characters. To win, your chosen character must defeat all the others. Each character has health points and attack points. The character you chose increases the damage points with each hit. 
+
+## Instructions
+
+1. Choose a characater.
+2. Choose a defender.
+3. Attack. 
+4. If your character loses, the game is over. 
+5. If your character prevails, choose another character.
+6. Attack again.
+7. If your character defeats all opponents, you win.
+
+## Built With
+
+* [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript) - Makes it interactive
+* [JQuery](http://jquery.com/) - Handles events
+* [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS) - Makes it pretty
+
+## Author
+
+* **Kris Acker Bierma** - [KrisBierma](https://github.com/KrisBierma)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- assets/javascript/game-v2.js
+++ assets/javascript/game-v2.js
@@ -1,35 +1,35 @@
 $(document).ready(function(){
 
-var marvin = {
+var marvin = { //luke
     name:"marvin",
     location: $("#marvin"),
-    hp: 50,
+    hp: 120,
     hpLocation: $("#marvin-hp"),
     damage: 8
     }
-var pepe = {
+var pepe = { //obi
     name:"pepe",
     location: $("#pepe"),
-    hp:75,
+    hp:130,
     hpLocation: $("#pepe-hp"),
-    damage: 12
+    damage: 10
     }
-var popeye = {
+var popeye = { //sidious
     name:"popeye",
     location: $("#popeye"),
-    hp:100,
+    hp:150,
     hpLocation: $("#popeye-hp"),
-    damage: 20
+    damage: 12
     }
-var rosie = {
+var rosie = { //maul
     name:"rosie",
     location: $("#rosie"),
-    hp:125,
+    hp:180,
     hpLocation: $("#rosie-hp"),
-    damage: 30
+    damage: 15
     }
 
-var info=[marvin, pepe, popeye, rosie];
+var info=[marvin, pepe, popeye, rosie]; //array of above character objects 
 var isCharChosen=false;
 var isDefenderChosen=false;
 var isDoneAttacking=false;
@@ -37,36 +37,27 @@ var isRestart=false;
 var currentDefender;
 var currentChar;
 var isEnemyDefeated=false;
+var enemiesLeft=3;
+var currentCharDam=0;
 
-
-// var infoClone = Object.assign({},info);
-// var infoClone = info.slice(0);
-// console.log(infoClone);
-
-function initializeGame(){
-    console.log("initialize is working");
-    isCharChosen=false;
-    isDefenderChosen=false;
-    isDoneAttacking=false;
-    isRestart=false;
-    isEnemyDefeated=false;
-    currentDefender="";
-    currentChar="";
-    for (var k=0; k<4; k++){
-        $("#characters").append(info[k].location);
-        $(info[k]).empty();
-        }
-    $("#yourChar, #enemies, #fight, #defender, #attackMsg, #restart").empty();
-    // info = infoClone.concat();
-    // let infoClone = [...info];
-    // info = Object.assign({}, infoClone);
-    // console.log(info);
-    // console.log(infoClone);
-    };  
+// function initializeGame(){
+//     console.log("initialize is working");
+//     isCharChosen=false;
+//     isDefenderChosen=false;
+//     isDoneAttacking=false;
+//     isRestart=false;
+//     isEnemyDefeated=false;
+//     currentDefender="";
+//     currentChar="";
+//     for (var k=0; k<4; k++){
+//         $("#characters").append(info[k].location);
+//         $(info[k]).empty();
+//         }
+//     $("#yourChar, #enemies, #fight, #defender, #attackMsg, #restart").empty();
+//     };  
 
 $(".charBox").on("click", function(event){
     if (isCharChosen) return;
-    // console.log(event.currentTarget.id); //delete later
     for (var i=0; i<4; i++){
         $(info[i].location).detach();
         if (info[i].name===event.currentTarget.id){
@@ -83,20 +74,16 @@ $(".charBox").on("click", function(event){
 
 $(document).on("click", ".charBox-enemies", function(){
     if (isDoneAttacking){
-        console.log("enemy down");
         enemyDown();
     }
     if (isDefenderChosen) {
-        console.log(".charBox defender chosen");
         return;}
     $("#attackMsg").empty();
     for (var j=0; j<4; j++){
         if (info[j].name===(event.path[1].id || event.path[2].id)){
-            console.log("running");
             $(info[j].location).detach();
             $("#defender").append(info[j].location);
             currentDefender=info[j];
-            console.log(currentDefender);
             (info[j].location).addClass("charBox-defender");
             (info[j].location).removeClass("charBox-enemies");
             isDefenderChosen=true;      
@@ -106,47 +93,34 @@ $(document).on("click", ".charBox-enemies", function(){
 
 $("#attack").on("click", function(){
     var charCapName = capFirstLetter(currentDefender.name);
+    currentCharDam = currentCharDam + currentChar.damage;
     if (!isCharChosen) return;
-    console.log(isDoneAttacking);
-    console.log(isDefenderChosen);
-    if (!isDefenderChosen) {
-        console.log("running");
-        return;}
+    if (!isDefenderChosen) return;
     if (currentChar.hp>0 && currentDefender.hp>0){
-        if (isDoneAttacking) {
-            console.log("running");
-            return;
-            }
+        if (isDoneAttacking) return; 
         else {
-            $("#attackMsg").html("You attacked " + charCapName +" for " + currentChar.damage + " damage." + "<br>"
+            $("#attackMsg").html("You attacked " + charCapName +" for " + currentCharDam + " damage." + "<br>"
             + charCapName + " attacked you back for " + currentDefender.damage + " damage.");
             currentChar.hp = currentChar.hp - currentDefender.damage;  
             $(currentChar.hpLocation).text(currentChar.hp);
-            currentDefender.hp = currentDefender.hp - currentChar.damage;
+            currentDefender.hp = currentDefender.hp - currentCharDam;
             $(currentDefender.hpLocation).text(currentDefender.hp);
-            currentChar.damage += currentChar.damage;
             }
         };
 
     if (currentDefender.hp<=0 && isDoneAttacking){
-        console.log("need this?"); //delete this section?
         $("#attackMsg").html("No enemy here.");
         }
     else if(currentDefender.hp<=0){
         $("#attackMsg").html("You have defeated " + charCapName +". Choose another enemy to fight.");
+        enemiesLeft--;
+        anyEnemiesLeft();
         isDoneAttacking = true;
         isEnemyDefeated=true;
         $(currentDefender.location).detach();
-        // enemyDown();
-        console.log("running");
-    } 
-        
+        }   
     else if (currentChar.hp<=0){
-        if (isRestart){
-            console.log("running");
-            return;
-            }
-            console.log("running");
+        if (isRestart) return;
         $("#attackMsg").html("You've been defeated...GAME OVER.");
         var restartButton=$("<button>");
         restartButton.text("Restart");
@@ -163,18 +137,21 @@ $("#attack").on("click", function(){
         isDefenderChosen=false;
         isDoneAttacking=false;
         isEnemyDefeated=false;
-        console.log("running");
         $("#attackMsg").empty();
         }
 
+    function anyEnemiesLeft (){
+        if (enemiesLeft===0){
+            $("#attackMsg").empty();
+            $("#winMsg").html("You have defeated all enemies! You win!");
+            $("#attack").remove();
+        }
+    }
+
 $("#restart").on("click", function(){
-    initializeGame();
+    // initializeGame();
+    window.location.reload();
     });
 
 // initializeGame();
    }); //end document.ready
-
-//choose another character
-//char attack power is still increasing
-//You won! Game over & restart button
-
